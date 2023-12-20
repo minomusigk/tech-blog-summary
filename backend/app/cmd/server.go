dc627488f28b1c33d1b28f1d7b1d6f8d4d73926b
minomusigk <isekaigk@gmail.com>
2023-12-20 13:43:22 +0900

    ginの導入

--- backend/app/cmd/server.go
+++ backend/app/cmd/server.go
@@ -1,8 +1,13 @@
 package main
 
-import "fmt"
+import "github.com/gin-gonic/gin"
 
 func main() {
-	name := "Go Developers"
-	fmt.Println("Azure for", name)
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080
 }
