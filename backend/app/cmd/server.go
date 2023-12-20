d6083b94c7850e35e416d656e338b6e7db00ea2c
minomusigk <isekaigk@gmail.com>
2023-12-20 14:50:08 +0900

    go.modの位置を修正し、controllerを追加

--- backend/app/cmd/server.go
+++ backend/app/cmd/server.go
@@ -1,13 +1,13 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/minomusigk/tech-blog-summary/backend/app/presentation/controller"
+)
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+
+	r.GET("/ping", controller.GetPing)
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
