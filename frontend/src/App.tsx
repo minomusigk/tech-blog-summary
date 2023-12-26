32ad4513c50a9b2327368160100dc9999c353f37
minomusigk <isekaigk@gmail.com>
2023-12-26 16:31:29 +0900

    画面からHTTP APIを実行できるよう対応

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,25 @@
-import React from 'react';
+import { useState } from 'react';
+import { Configuration, GetPingApiFactory } from './api/axios';
 
-const App = (): JSX.Element => <div>http</div>;
+const App = (): JSX.Element => {
+  const [message, setMessage] = useState('null');
+
+  const apiConfig = new Configuration({
+    basePath: 'http://localhost:8080',
+  });
+  const client = GetPingApiFactory(apiConfig);
+
+  const onPingButtonClick = async () => {
+    const result = await client.getPing();
+    setMessage(result.data.message);
+  };
+
+  return (
+    <div>
+      <button onClick={() => onPingButtonClick()}>ping</button>
+      <div>{message}</div>
+    </div>
+  );
+};
 
 export default App;
